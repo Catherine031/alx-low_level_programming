af6b350fd03471359c0ec818a8d27d0e656379db
Catherine <mwilacm03@gmail.com>
2023-02-17 09:04:42 -0800

    MainTask1

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,7 @@
 /**
  * main - main block
  * Description: print the alphabet in lower case
- * and the in uppercase, follow by a new line
+ * and in the upper case, follow by a new line
  * Return: 0
  */
 int main(void)
@@ -22,4 +22,7 @@ int main(void)
 		putchar(c);
 		c++;
 	}
+
+	putchar('\n');
+	return (0);
 }
