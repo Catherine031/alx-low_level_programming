07c9bf2d28cfd2112794e16bd7c550db82f088c2
Catherine <mwilacm03@gmail.com>
2023-02-17 11:55:45 -0800

    MainTasks3

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - main block
+ * Description: prints all single number of base 10
+ * starting from 0, followed by a new line
+ * Return: 0
+ */
 int main(void)
 {
-	int c =0;
+	int c = 0;
+
 	while (c < 10)
 	{
 		putchar(48 + c);
@@ -14,6 +20,7 @@ int main(void)
 		}
 		c++;
 	}
+
 	putchar('\n');
 	return (0);
 
